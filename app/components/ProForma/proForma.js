e1277ce4d2bdb7266f745ba4391bdcc1ee821101
Paul Turner Blatchley <ptblatchley@gmail.com>
2017-05-23 16:54:51 -0700

    Added condition for setting all defaults if proforma table is completely empty

--- app/components/ProForma/proForma.js
+++ app/components/ProForma/proForma.js
@@ -12,7 +12,6 @@ app.component(
         controller: function($scope, auth, $http, singleproperty, proforma) {
             // Get Property Info
         	$scope.s = singleproperty.property;
-
             $scope.proforma = {};
 
             // Convert Currency Strings to Numbers
@@ -75,31 +74,52 @@ app.component(
 
             $scope.proforma.profit_share = 100;
 
-            for(i = 0; i < proforma.proforma.length; i ++) {
-                if (proforma.proforma[i].pid == $scope.s.pid) {
-                    for (j in proforma.proforma[i]) {
-                        $scope.proforma[j] = proforma.proforma[i][j];
+            if (proforma.proforma.length) {
+                for(i = 0; i < proforma.proforma.length; i ++) {
+                    if (proforma.proforma[i].pid == $scope.s.pid) {
+                        for (j in proforma.proforma[i]) {
+                            $scope.proforma[j] = proforma.proforma[i][j];
+                        }
+                    } else {
+                        // Set Defaults
+                        $scope.default = proforma.defaults;
+                        $scope.proforma.loan_amount = $scope.proforma.arv * .7;
+                        $scope.proforma.apr = $scope.default.apr;
+                        $scope.proforma.months = $scope.default.months;
+                        $scope.proforma.fees = $scope.default.fees;
+                        $scope.proforma.opening_points = $scope.default.opening_points;
+                        $scope.proforma.other_costs = 0;
+                        $scope.proforma.selling_costs_percent = $scope.default.selling_closing_costs_percent;
+                        $scope.proforma.buyer_percent = $scope.default.buyers_contribution;
+                        $scope.proforma.tc = $scope.default.tc;
+                        $scope.proforma.accounting = $scope.default.accounting;
+                        $scope.proforma.tca = ($scope.proforma.tc*1) + ($scope.proforma.accounting*1);
+                        $scope.proforma.commission_percent = $scope.default.commission;
+                        $scope.proforma.jeremy_pocket = 0;
+                        $scope.proforma.codrin_pocket = 0;
+                        $scope.proforma.tetakawi_share_percent = $scope.default.tetakawi_share;
                     }
-                } else {
-                    // Set Defaults
-                    $scope.default = proforma.defaults;
-                    $scope.proforma.loan_amount = $scope.proforma.arv * .7;
-                    $scope.proforma.apr = $scope.default.apr;
-                    $scope.proforma.months = $scope.default.months;
-                    $scope.proforma.fees = $scope.default.fees;
-                    $scope.proforma.opening_points = $scope.default.opening_points;
-                    $scope.proforma.other_costs = 0;
-                    $scope.proforma.selling_costs_percent = $scope.default.selling_closing_costs_percent;
-                    $scope.proforma.buyer_percent = $scope.default.buyers_contribution;
-                    $scope.proforma.tc = $scope.default.tc;
-                    $scope.proforma.accounting = $scope.default.accounting;
-                    $scope.proforma.tca = ($scope.proforma.tc*1) + ($scope.proforma.accounting*1);
-                    $scope.proforma.commission_percent = $scope.default.commission;
-                    $scope.proforma.jeremy_pocket = 0;
-                    $scope.proforma.codrin_pocket = 0;
-                    $scope.proforma.tetakawi_share_percent = $scope.default.tetakawi_share;
                 }
+            } else {
+                // Set Defaults
+                $scope.default = proforma.defaults;
+                $scope.proforma.loan_amount = $scope.proforma.arv * .7;
+                $scope.proforma.apr = $scope.default.apr;
+                $scope.proforma.months = $scope.default.months;
+                $scope.proforma.fees = $scope.default.fees;
+                $scope.proforma.opening_points = $scope.default.opening_points;
+                $scope.proforma.other_costs = 0;
+                $scope.proforma.selling_costs_percent = $scope.default.selling_closing_costs_percent;
+                $scope.proforma.buyer_percent = $scope.default.buyers_contribution;
+                $scope.proforma.tc = $scope.default.tc;
+                $scope.proforma.accounting = $scope.default.accounting;
+                $scope.proforma.tca = ($scope.proforma.tc*1) + ($scope.proforma.accounting*1);
+                $scope.proforma.commission_percent = $scope.default.commission;
+                $scope.proforma.jeremy_pocket = 0;
+                $scope.proforma.codrin_pocket = 0;
+                $scope.proforma.tetakawi_share_percent = $scope.default.tetakawi_share;
             }
+            
 
             $scope.addItem = function(item) {
                 var cost = {};
